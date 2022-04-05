5162a9e191e574245601579db8e545115a3cac8d
Sunilganwane <sunilganwane@example.com>
2022-04-05 13:15:01 +0530

    testunig

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import Header from './Header';
+// import Header from './Header';
 import HeroSection from './Components/HeroSection'
 import FeaturedCategory from './Components/FeaturedCategory';
 import PopularCategory from './Components/PopularCategory';
@@ -24,7 +24,7 @@ function Home() {
   return (
     <>
 
-     <Header />
+     {/* <Header /> */}
 
 <main>
      <HeroSection />
