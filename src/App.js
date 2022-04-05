301d314f37644a61df1751a6ed8eed52e1ff27b6
Sunilganwane <sunilganwane@example.com>
2022-04-05 12:05:56 +0530

    sunil

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          sunil ganwane <code>src/App.js</code> and save to reload.
+          Sumit github pages  <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
