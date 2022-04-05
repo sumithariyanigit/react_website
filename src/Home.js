93337f37a417cca2d011c963b9db9c8162795d65
Sunilganwane <sunilganwane@example.com>
2022-04-05 12:18:28 +0530

    cleaning

---
+++ src/Home.js
@@ -0,0 +1,54 @@
+import React, {useEffect} from 'react';
+import Header from './Header';
+import HeroSection from './Components/HeroSection'
+import FeaturedCategory from './Components/FeaturedCategory';
+import PopularCategory from './Components/PopularCategory';
+import RecentBlogs from './Components/RecentBlogs';
+import Testimonial from './Components/Testimonial';
+import Footer from './Footer';
+import Subscribe from './Components/Subscribe';
+import { Outlet, Link } from "react-router-dom";
+import BestOffers from './Components/BestOffers';
+
+
+function Home() {
+ 
+  useEffect( () => {
+    const body = document.querySelector("body");
+    document.body.classList.add("body_header");
+    return () => {
+      body.classList.remove("body_header");
+    }
+  } );
+
+ 
+  return (
+    <>
+
+     <Header />
+
+<main>
+     <HeroSection />
+
+     <FeaturedCategory />
+
+     <PopularCategory />
+
+     <BestOffers />     
+
+     <RecentBlogs />
+     
+     <Testimonial />
+
+     <Subscribe />   
+
+     <Footer />
+</main>
+
+
+
+    </>
+  );
+}
+
+export default Home;
