031df68ec35764c494b5cccb7d5f371e9bf5a020
Sunilganwane <action@github.com>
2022-04-05 13:25:13 +0530

    ttt

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom";
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import run from '@jamesives/github-pages-deploy-action'
+
 // import About from "./About";
 // import Blogs from "./Blogs";
 // import Packages from "./Packages";
